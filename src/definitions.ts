f7eef2de52880ec6cbe308e1c12888e8c9a51870
Olivier <olivier@overstraete.be>
2020-05-28 15:00:10 +0200

    Typescript definitions

--- src/definitions.ts
+++ src/definitions.ts
@@ -5,6 +5,36 @@ declare module "@capacitor/core" {
 }
 
 export interface BlinkIDPlugin {
+  setLicenseKey(licenseKey: string): Promise<void>;
+  startScanning(options: { recognizers: Recognizer[] }): Promise<ScanningStatus>;
+}
 
-  show(options?: {}): Promise<{ status: string, message: string, data: any }>;
+class Recognizer {
+  document: RecognizerDocument;
+  returnFullDocumentImage = true;
+  returnFaceImage = true;
+  allowUnverifiedResults = false;
+  allowUnparsedResults = false;
+  result: ResultState;
+
+  constructor(document: RecognizerDocument) {
+    this.document = document;
+    this.result = ResultState.Empty;
+  }
+}
+
+enum ScanningStatus {
+  Cancelled,
+  Succeeded
+}
+
+enum RecognizerDocument {
+  ID,
+  Passport,
+  Combined
+}
+
+enum ResultState {
+  Empty,
+  Valid
 }
